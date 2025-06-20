485ab83d99f81dd883eff18ad061a15abcff1946
kashefbasher <30617855+kashefbasher@users.noreply.github.com>
2025-06-19 21:19:44 -0400

    terraform files for ec2

---
+++ commands.sh
@@ -0,0 +1,15 @@
+mkdir terraformProject
+cd terraformProject/
+touch main.tf
+touch provider.tf
+touch terraform.tfvars
+touch variable.tf
+
+
+
+#aws configure
+#terraform init
+#terraform fmt
+#terraform plan
+#terraform apply
+#terraform destroy
